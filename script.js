a38d48e744e655a7a8bc81a9be6c2e648d3a4749
zabeth <111363917+Javedzuck12@users.noreply.github.com>
2024-10-19 12:02:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+let expenses = [];
+let totalAmount = 0;
+//let modeBtn=document.querySelector('#mode');
+//let currMode="light";//dark
+
+/*modeBtn.addEventListener("click",()=>{
+    if(currMode=="light"){
+        currMode="dark";
+        document.querySelector("body").style.backgroundColor="light";
+    }else{
+        currMode="light";
+        document.querySelector("body").style.backgroundColor="dark";
+    }
+    console.log(currMode);
+})*/
+
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-input');
+const dateInput = document.getElementById('date-input');
+const addbtn= document.getElementById('add-btn');
+const expensesTableBody = document.getElementById('expenses-table-body');
+const totalAmountCell = document.getElementById('total-amount');
+
+
+    addbtn.addEventListener('click', function() {
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
+
+    if (category === '') {
+        alert('Please select a category');
+        return;
+    }
+    if (isNaN(amount) || amount <=0 ) {
+        alert('Please enter a valid amoun')
+        return;
+    }
+    if(date === '') {
+        alert('Please select a date')
+        return;
+    }
+    expenses.push({category, amount, date});
+
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow=expensesTableBody.insertRow();
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function() {
+        expenses.splice(expenses.indexOf(expense),1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTableBody.removeChild(newRow);
+    });
+
+    const expense = expenses[expenses.length - 1];
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
+
+});
+
+for (const expense of expenses) {
+    totalAmount += expense.amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTableBody.inserRow();
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function() {
+        expenses.splice(expenses.indexOf(expense), 1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTableBody.removeChild(newRow);
+    });
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
+};
